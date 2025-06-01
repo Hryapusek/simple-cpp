697e1c3ddbe24ba0629b7946f36f77abfab401fe
Abraamyan Alexander <samvelcc25@outlook.com>
2025-06-01 18:52:37 +0300

    server updated

--- cpp/43. examples/qt_tcp_server/server/src/c++/example-qt-tcp-server/server/server.h
+++ cpp/43. examples/qt_tcp_server/server/src/c++/example-qt-tcp-server/server/server.h
@@ -5,12 +5,32 @@
 #include <memory>
 #include <QAbstractEventDispatcher>
 
+/*
+1. Мы запустили сервер (запросили у системы сокет(1) для прослушивания и принятия входящих подключений)
+2. Клиент отправляет серверу запрос на соединение
+3. ОС выделила серверу ДРУГОЙ сокет для общения с клиентом (Так как сокет(1) на котором работает сервер уже занят, 
+   для взаимодействия с клиентом нужен другой сокет)
+4. Handshake
+--------- Дальше в зависимости от сервера. Например:
+5. Сервер ждет запрос от клиента
+6. При получении запроса происходит обработка и отправка ответа клиенту
+7. И так пока не будет получен запрос на разрыв соединения
+
+# Сокеты Бернулли
+- read
+- write
+- accept
+- listen
+- connect
+- disconnect
+*/
+
 class ClientHandler : public QThread
 {
   Q_OBJECT
 
  public:
-  explicit ClientHandler(qintptr socketDescriptor)
+  explicit ClientHandler(long long socketDescriptor)
     : m_socketDescriptor(socketDescriptor)
   {}
 
@@ -23,23 +43,34 @@ class ClientHandler : public QThread
       return;
     }
 
-    connect(socket.get(), &QTcpSocket::readyRead, eventDispatcher(), [this, socket]() { 
+    connect(socket.get(), &QTcpSocket::readyRead, this->eventDispatcher(), [this, socket]() { 
       handleReadyRead(socket);
     });
 
-    connect(socket.get(), &QTcpSocket::disconnected, eventDispatcher(), [this, socket]() { 
+    // connect(socket.get(), &QTcpSocket::readyRead, this->eventDispatcher(), foo);
+    // connect(socket.get(), &QTcpSocket::readyRead, this->eventDispatcher(), foo());
+
+    connect(socket.get(), &QTcpSocket::disconnected, this->eventDispatcher(), [this, socket]() { 
       handleDisconnected(socket);
     });
 
+    // socket->disconnectFromHost();
+
+    // this;
+
     exec();  // Start the event loop for this thread
+    /*
+    start -> system thread -> run()
+    run in different thread
+    */
   }
   
-  qintptr m_socketDescriptor;
+  long long m_socketDescriptor;
 
- signals:
+ signals: // public
   void error(QTcpSocket::SocketError socketError);
 
-  private slots:
+  private slots: // slots -> "" -> nothing
   void handleReadyRead(std::shared_ptr<QTcpSocket> socket)
   {
     QByteArray data = socket->readAll();
@@ -51,10 +82,15 @@ class ClientHandler : public QThread
     }
   }
 
+  // Вызывается при закрытии соединения
   void handleDisconnected(std::shared_ptr<QTcpSocket> socket)
   {
+    // Возвращаем системе ресурс
     socket->close();
-    quit();
+    // Завершаем работу потока
+    // Каждый поток рассчитан на ОДНО соединение потому что мы сами так решили
+    // Как только соединение разрывается - поток больше не нужен
+    this->quit(); // Exiting thread
   }
 };
 
